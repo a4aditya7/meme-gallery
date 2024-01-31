d2ebbac034a717005e83d392f4f2421836ad7cff
a4aditya7 <adityamarandi36@gmail.com>
2024-01-31 23:10:41 +0530

    uploaded initial files

---
+++ app/components/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () =>{
+    return (
+        <nav className="navbar">
+        <div className="navbar-title">Meme Gallery</div>
+        <img src="https://www.pngarts.com/files/8/Github-Logo-Transparent-Background-PNG.png" className="navbar-image"></img>
+        {/* <a href=""></a> */}
+        {/* Add additional links or components here if needed */}
+      </nav>
+    );
+}
+export default Navbar;
